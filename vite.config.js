0f6172bc84e180009fc14446f6bc80c953e59001
Jampier <jeampierre.pacori@intitecperu.com>
2026-02-01 10:19:05 -0500

    Set Vite base to /My-portfolio/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/My-portfolio/',
   publicDir: 'images',
   server: {
     host: true,
