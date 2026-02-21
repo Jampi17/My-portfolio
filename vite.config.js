871fe6be0b1a0b3703c22c1212ce2da7de23503d
Jampier <jeampierre.pacori@intitecperu.com>
2026-02-21 13:04:35 -0500

    [FEAT]: Mejoras

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/My-portfolio/',
+  base: '/react-portfolio/',
   publicDir: 'images',
   server: {
     host: true,
